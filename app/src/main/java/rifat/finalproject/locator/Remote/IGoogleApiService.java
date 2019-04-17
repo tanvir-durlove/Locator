f0da1b54c3892a479703b559b784ee92823f8437
tanvir-durlove <tanvirahameddurlove@gmail.com>
2019-04-17 10:42:47 +0600

    Bug Fix on Button click

--- app/src/main/java/rifat/finalproject/locator/Remote/IGoogleApiService.java
+++ app/src/main/java/rifat/finalproject/locator/Remote/IGoogleApiService.java
@@ -1,10 +1,14 @@
 package rifat.finalproject.locator.Remote;
 
 import retrofit2.Call;
+import retrofit2.http.GET;
 import retrofit2.http.Url;
 import rifat.finalproject.locator.Model.MyPlaces;
 
 public interface IGoogleApiService {
 
+
+
+    @GET
     Call<MyPlaces>getNearByPlaces(@Url String url);
 }
