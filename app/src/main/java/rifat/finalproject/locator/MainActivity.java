167cabe575db895d0f3ef4acb797239146e651c8
Tanvir Durlove <tanvirahameddurlove@gmail.com>
2019-04-18 01:51:16 +0600

    Add Feedback system through email

--- app/src/main/java/rifat/finalproject/locator/MainActivity.java
+++ app/src/main/java/rifat/finalproject/locator/MainActivity.java
@@ -1,14 +1,16 @@
 package rifat.finalproject.locator;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    RelativeLayout offline_content;
+    RelativeLayout offline_content,feedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, OfflineContent.class);
                 finish();
                 startActivity(i);
+            }
+        });
+
+        feedback = (RelativeLayout)findViewById(R.id.feedback);
+        feedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Intent.ACTION_SENDTO);
+                i.setData(Uri.parse("mailto:"));
+//                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{"admin.locator@gmail.com"});
+                i.putExtra(Intent.EXTRA_SUBJECT, "Feedback for Locator");
+                i.putExtra(Intent.EXTRA_TEXT   , "");
+                try {
+                    startActivity(Intent.createChooser(i, "Choose for sending feedback."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
